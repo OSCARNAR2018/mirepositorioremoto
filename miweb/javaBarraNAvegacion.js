e3a92d94a6a089eaf0263efd7fa0ed7a22282ac6
EggCooperation Oscar Narduzzi <oscar.narduzzi@gmail.com>
2023-07-03 22:35:34 -0300

    cambio mi primer website

---
+++ miweb/javaBarraNAvegacion.js
@@ -0,0 +1,32 @@
+#eader {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+  }
+  
+#nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  
+#nav li {
+    display: inline-block;
+    margin-right: 20px;
+  }
+  
+  nav a {
+    color: #fff;
+    text-decoration: none;
+    padding: 5px;
+  }
+
+  nav a:hover {
+    color: red;
+  }
+
+@media (max-width: 767px) {
+    header
+    background-color: blue;
+    }
